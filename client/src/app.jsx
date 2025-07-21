e70d491592d7402843c4c27ed13c751e6074ab0e
Anku <ankitkumarmuhal3@gmail.com>
2025-07-22 00:47:05 +0530

    refactor: splitting the components done

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router";
 import ItemPage from "./pages/ItemsPage";
-import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<ItemPage />} />
-        <Route path="/test" element={<HomePage />} />
       </Routes>
     </Router>
   )
